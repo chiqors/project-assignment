0d0dae8034228c2884a1e5ed63ad51444631ee3e
Chiqo Rifky Saputra <chiqors@outlook.com>
2023-01-20 17:49:56 +0700

    dev: tambahan untuk membuat API backend

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -11,4 +11,14 @@ class Client extends Model
 
     public $timestamps = false;
     protected $table = 'tb_m_client';
+
+    protected $fillable = [
+        'client_name',
+        'client_address'
+    ];
+
+    public function project()
+    {
+        return $this->hasMany('App\Models\Project','client_id');
+    }
 }
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -11,4 +11,17 @@ class Project extends Model
 
     public $timestamps = false;
     protected $table = 'tb_m_project';
+
+    protected $fillable = [
+        'project_name',
+        'client_id',
+        'project_start',
+        'project_end',
+        'project_status'
+    ];
+
+    public function client()
+    {
+        return $this->belongsTo('App\Models\Client','client_id');
+    }
 }
